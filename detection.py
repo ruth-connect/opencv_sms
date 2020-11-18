944a1411b0db74a5e50a42116d21a1b62938fbb8
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-18 20:47:01 +0000

    Try getting weight for faces.

--- detection.py
+++ detection.py
@@ -16,37 +16,21 @@ while (True):
     frame = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
 
     # pass the frame to the classifier
-    faces_detected, faces_weights = faceClassifier.detectMultiScale(frame, 1.1, 3)
-    upper_bodies_detected, upper_bodies_weights = upperBodyClassifier.detectMultiScale(frame, 1.1, 3)
-    full_bodies_detected, full_bodies_weights = fullBodyClassifier.detectMultiScale(frame, 1.1, 3)
+    faces = faceClassifier.detectMultiScale3(frame, 1.1, 3)
+    upper_bodies = upperBodyClassifier.detectMultiScale3(frame, 1.1, 3)
+    full_bodies = fullBodyClassifier.detectMultiScale3(frame, 1.1, 3)
 	
     # how many faces have been detected on the frame
     try:
-        face_count = faces_detected.shape[0]
-        face_weight = faces_weights[0]
+        face_rects = faces[0]
+        face_weights = faces[2]
+        face_count = len(face_rects)
+        face_weight = max(face_weights)
     except:
         face_count = 0
         face_weight = 0
 	
-# how many upper bodies have been detected on the frame
-    try:
-        upper_body_count = upper_bodies_detected.shape[0]
-        upper_body_weight = upper_body_weights[0]
-    except:
-        upper_body_count = 0
-        upper_body_weight = 0
-        
-    # how many full bodies have been detected on the frame
-    try:
-        full_body_count = full_bodies_detected.shape[0]
-        full_body_weight = full_body_weights[0]
-    except:
-        full_body_count = 0
-        full_body_weight = 0
-
     print (str(face_count) + ' faces detected with weight ' + face_weight)
-    print (str(upper_body_count) + ' upper bodies detected with weight ' + upper_body_weight)
-    print (str(full_body_count) + ' full bodies detected with weight ' + full_body_weight)
 
     time.sleep(1)
     
