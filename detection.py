980e9eec38397c565003ed10495be9d9c5cf933e
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-18 20:17:06 +0000

    Try reading image from HAL again.

--- detection.py
+++ detection.py
@@ -8,8 +8,10 @@ fullBodyClassifier = cv2.CascadeClassifier('./haarcascade_upperbody.xml')
 
 while (True):
 	
-    # read frame-by-frame
-    frame = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE)
+	# get image from url
+    response = urllib.urlopen('http://192.168.1.115/mjpeg_read.php')
+    image = np.asarray(bytearray(response.read()), dtype="uint8")
+    frame = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
 
     # pass the frame to the classifier
     faces_detected = faceClassifier.detectMultiScale(frame, 1.1, 3)
@@ -18,21 +20,21 @@ while (True):
 	
     # how many faces have been detected on the frame
     try:
-	    face_count = faces_detected.shape[0]
+        face_count = faces_detected.shape[0]
     except:
-	    face_count = 0
+        face_count = 0
 	
 # how many upper bodies have been detected on the frame
     try:
-	    upper_body_count = upper_bodies_detected.shape[0]
+        upper_body_count = upper_bodies_detected.shape[0]
     except:
-	    upper_body_count = 0
+        upper_body_count = 0
 
     # how many full bodies have been detected on the frame
     try:
-    	full_body_count = full_bodies_detected.shape[0]
+        full_body_count = full_bodies_detected.shape[0]
     except:
-    	full_body_count = 0
+        full_body_count = 0
 
     print (str(face_count) + ' faces detected')
     print (str(upper_body_count) + ' upper bodies detected')
