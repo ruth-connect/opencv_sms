e2d055b5f1d5bc904fff43bbfc509bc2bad563e3
ruth <ruth@DESKTOP-HOVL7PT>
2020-11-18 21:06:08 +0000

    Always log even if zero.

--- detection.py
+++ detection.py
@@ -5,20 +5,19 @@ import numpy
 
 def detect(frame, classifier, type):
 
-	results = classifier.detectMultiScale3(frame, 1.1, 3, outputRejectLevels=True)
+    results = classifier.detectMultiScale3(frame, 1.1, 3, outputRejectLevels=True)
 	
-	# how many faces have been detected on the frame
-	try:
-		rects = results[0]
-		weights = results[2]
-		count = len(rects)
-		weight = max(weights)
-	except:
-		count = 0
-		weight = 0
+    # how many faces have been detected on the frame
+    try:
+        rects = results[0]
+        weights = results[2]
+        count = len(rects)
+        weight = max(weights)
+    except:
+        count = 0
+        weight = 0
 	
-	if (weight >= 2):
-	    print (str(count) + ' ' + type + ' detected with weight ' + str(weight))
+    print (str(count) + ' ' + type + ' detected with weight ' + str(weight))
 
 # load pre-trainer classifier
 faceClassifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
@@ -27,7 +26,7 @@ fullBodyClassifier = cv2.CascadeClassifier('./haarcascade_upperbody.xml')
 
 while (True):
 	
-	# get image from url
+    # get image from url
     response = urllib.request.urlopen('http://192.168.1.115/mjpeg_read.php')
     image = numpy.asarray(bytearray(response.read()), dtype="uint8")
     frame = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
